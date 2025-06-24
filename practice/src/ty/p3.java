4e19c4c3165b1c96accd11cf3a0b54228d8c02d2
lenovo <lenovo@DESKTOP-O9L4K3A>
2025-06-24 18:05:35 +0530

    This is 3rd class I am commiting

---
+++ practice/src/ty/p3.java
@@ -0,0 +1,11 @@
+package ty;
+
+public class p3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git this is my 3rd class I am pushing");
+
+	}
+
+}
