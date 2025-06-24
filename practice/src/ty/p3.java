58eb3f9f05f79acbba61d1c7e88b506ffe9e7051
Abhi-1993-abd <60272376+Abhi-1993-abd@users.noreply.github.com>
2025-06-24 18:07:23 +0530

    Update p3.java

--- practice/src/ty/p3.java
+++ practice/src/ty/p3.java
@@ -5,6 +5,7 @@ public class p3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git this is my 3rd class I am pushing");
+		System.out.println("This were the changes I made from git repo");
 
 	}
 
